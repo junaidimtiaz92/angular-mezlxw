2cb20f6e78c40f075913ce30d6ff403b09f19278
junaidimtiaz92 <junaidimtiaz92@gmail.com>
2022-04-26 14:42:49 +0500

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -6,7 +6,7 @@ import { Product, products } from '../products';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'] 
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
